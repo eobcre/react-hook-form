f2830053a7f9f354d407ae9afc67d52e4324dd97
eobcre <eobcre@gmail.com>
2023-06-05 23:23:25 -0400

    basic usage settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
+import { useForm } from 'react-hook-form';
+
 const App = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor='name'>Name</label>
-        <input type='text' name='name' className='form-border' />
+        <input
+          type='text'
+          id='name'
+          className='form-border'
+          {...register('name')}
+        />
 
         <label htmlFor='email'>Email</label>
-        <input type='email' name='email' className='form-border' />
+        <input
+          type='email'
+          id='email'
+          className='form-border'
+          {...register('email')}
+        />
 
         <label htmlFor='password'>Password</label>
-        <input type='password' name='password' className='form-border' />
+        <input
+          type='password'
+          id='password'
+          className='form-border'
+          {...register('password')}
+        />
 
         <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
           Login
