49f7853000f06bbc2540e3d03c0be6c02753e9da
eobcre <eobcre@gmail.com>
2023-06-05 23:07:29 -0400

    created login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <form>
+        <label htmlFor='name'>Name</label>
+        <input type='text' name='name' className='form-border' />
+
+        <label htmlFor='email'>Email</label>
+        <input type='email' name='email' className='form-border' />
+
+        <label htmlFor='password'>Password</label>
+        <input type='password' name='password' className='form-border' />
+
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+          Login
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
