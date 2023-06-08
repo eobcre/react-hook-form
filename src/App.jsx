8e0c0afca622007b3314e6bd4ad00afbcd81f63d
eobcre <eobcre@gmail.com>
2023-06-07 22:58:22 -0400

    button margin changed, error message changed

--- src/App.jsx
+++ src/App.jsx
@@ -20,29 +20,29 @@ const App = () => {
           type='text'
           id='name'
           className='form-border'
-          {...register('name', { required: 'Name is required.' })}
+          {...register('name', { required: '* Name is required.' })}
         />
-        <p className='text-red-500'>{errors.name?.message}</p>
+        <p className='text-red-500 mb-3'>{errors.name?.message}</p>
 
         <label htmlFor='email'>Email</label>
         <input
           type='email'
           id='email'
           className='form-border'
-          {...register('email', { required: 'Email is required.' })}
+          {...register('email', { required: '* Email is required.' })}
         />
-        <p className='text-red-500'>{errors.email?.message}</p>
+        <p className='text-red-500 mb-3'>{errors.email?.message}</p>
 
         <label htmlFor='password'>Password</label>
         <input
           type='password'
           id='password'
           className='form-border'
-          {...register('password', { required: 'Password is required.' })}
+          {...register('password', { required: '* Password is required.' })}
         />
-        <p className='text-red-500'>{errors.password?.message}</p>
+        <p className='text-red-500 mb-4'>{errors.password?.message}</p>
 
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4 mt-4 w-full'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4 w-full'>
           Login
         </button>
       </form>
