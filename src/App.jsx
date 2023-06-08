eacd0c1ed97e50f47018739b2484e2f990594938
eobcre <eobcre@gmail.com>
2023-06-07 22:48:07 -0400

    login form layout and button styled

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
     console.log(data);
   };
   return (
-    <div>
+    <div className='h-screen flex flex-col justify-center items-center'>
       <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor='name'>Name</label>
         <input
@@ -40,7 +40,7 @@ const App = () => {
         />
         <p className='text-red-500'>{errors.password?.message}</p>
 
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold rounded py-2 px-4 mt-4 w-full'>
           Login
         </button>
       </form>
